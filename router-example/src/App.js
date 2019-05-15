3bba07c1f898280c2f64c0bff05ac3916f8319ed
bhague1281 <bhague1281@gmail.com>
2019-05-14 19:31:09 -0700

    more router examples

--- router-example/src/App.js
+++ router-example/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
 
 function Home(props) {
-  console.log(props);
   return (
     <div>
       <Link to="/about">About</Link>
@@ -12,8 +16,13 @@ function Home(props) {
   );
 }
 
-function About() {
-  return <h1>About</h1>
+function About(props) {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>{props.content}</p>
+    </div>
+  );
 }
 
 function NotFound() {
@@ -21,20 +30,39 @@ function NotFound() {
 }
 
 function Dog(props) {
-  console.log(props.match.params);
-  return <h1>Dog</h1>;
+  console.log(props);
+  return <h1>{props.match.params.breed}</h1>;
 }
 
+function WrappedAbout(routerProps) {
+  return <About {...routerProps} content="lorem ipsum" />
+}
+
+const breedList = [
+  'Retriever',
+  'Poodle',
+  'Hound',
+  'Bulldog'
+];
+
 function App() {
+  const breedLinks = breedList.map((breed, idx) => {
+    return <Link key={idx} to={`/dogs/${breed}`}>{breed}</Link>
+  });
+
   return (
     <div>
       <Router>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
 
+        <hr />
+        {breedLinks}
+
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          {/* <Route path="/about" component={About} /> */}
+          <Route path="/about" render={WrappedAbout} />
           <Route path="/dogs/:breed" component={Dog} />
           <Route component={NotFound} />
         </Switch>
